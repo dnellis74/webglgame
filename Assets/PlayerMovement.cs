2ffd80fdb1634e41f04a33199fc84dac5a2e33c1
David Nellis <dnellis74@gmail.com>
2020-04-13 04:13:00 -0600

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigidbody2D;
+
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+        Vector3 lTemp = transform.localScale;
+        if (movement.x > 0)
+        {
+            lTemp.x = 1;
+
+        }
+        else
+        {
+            lTemp.x = -1;
+        }
+       
+        transform.localScale = lTemp;
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.MovePosition(rigidbody2D.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
